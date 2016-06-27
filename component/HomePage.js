454e29df9fcff81046feb967974cdd60474b9afa
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-27 14:49:44 +0100

    YL -add content product character in homepage

--- component/HomePage.js
+++ component/HomePage.js
@@ -49,7 +49,22 @@ class HomePage extends React.Component {
                         <img src="../assert/image/ic_favorite.png"/>
                     </div>
 
-                    <div className="divider">
+                    <div className="divider"></div>
+
+                    <div className="product-character">
+                        <h3 className="character-title">产品特点:</h3>
+
+                        <div className="characters">
+                            <ul>
+                                <li>1、专业设计：阻力小，速度快，防渗漏，性能稳定。</li>
+                                <li>2、专用电机：低噪音，低耗能，寿命长。</li>
+                                <li>3、专用电路：无级调速，超强抗干扰，控制精准。</li>
+                                <li>4、专用遥控：多条船同时航行互不干扰，遥控距离达500米。</li>
+                                <li>5、玻璃钢船体：耐腐蚀，抗氧化，经久耐用。</li>
+                            </ul>
+
+                            <img src="../assert/image/ic_favorite.png" alt=""/>
+                        </div>
                     </div>
                 </content>
                 
