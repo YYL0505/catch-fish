ef949cca6ae31d192576228fdbeb2ad39b6bcf35
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-27 10:27:37 +0100

    YL - init project

---
+++ HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HomePage extends React.Component {
+
+    render() {
+        return (
+            <h1>Catch Fish</h1>
+    );
+    }
+}
+
+export default HomePage
