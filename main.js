83664c2a695581063bd2cb7ef7f2ee48d04393b1
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-28 15:33:22 +0100

    YL - use custom hashHistory to fix the 404 when reloading

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, useRouterHistory} from 'react-router'
+import {createHashHistory} from 'history'
+
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 import App from './component/App'
 import HotProduct from './component/HotProduct'
 import CompanyInfo from './component/CompanyInfo'
 
 render((
-    <Router history={browserHistory}>
+    <Router history={appHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={HotProduct}/>
             <Route path="/company-info" component={CompanyInfo}/>
